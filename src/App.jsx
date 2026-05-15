9e41b16b65da7abb8395565b1fced64ff2760fc1
Garden Fiend <rainethegardenfiend@gmail.com>
2026-05-15 21:26:05 +0800

    a few edits

--- src/App.jsx
+++ src/App.jsx
@@ -5,72 +5,85 @@ import { Entries } from "../public/data/journalEntries";
 
 function App() {
   const [page, setPage] = useState("Home");
+  const [opacity, setOpacity] = useState(0);
 
   return (
-    <div className="w-full  bg-[#F5E7C6]">
-      <nav className="w-full fixed bg-[#F5E7C6] z-100 top-0">
-        <div className="flex items-center px-6">
-          <img src="/fis.png" className="active:rotate-360 transition-transform duration-200 w-10 rounded-full" onClick={()=> setPage("Home")}></img>
-          <div className="flex justify-end w-full p-4 md:space-x-20 gap-4 font-mono font-bold items-center md:text-lg text-sm">
-            <p
-              onClick={() => {
-                setPage("Home");
-              }}
-              className="hover:border-b-2 transition-all duration-50"
-            >
-              Home
-            </p>
-            <p
-              onClick={() => {
-                setPage("Artworks");
-              }}
-              className="hover:border-b-2 transition-all duration-50"
-            >
-              Artworks
-            </p>
-          </div>
-        </div>
-      </nav>
-
+    <div className="w-full  bg-[#F5E7C6] relative">
       {page === "Home" && (
-        <div className="w-full pt-15 pb-1  bg-[#F5E7C6]">
+        <div className="w-full  pb-1  bg-[#e9e4d9]">
           <div className="relative w-full">
+            <nav className="w-full fixed bg-transparent z-100 text-white">
+              <div className="flex items-center px-6">
+                <img
+                  src="/fis.png"
+                  className="active:rotate-360 transition-transform duration-200 w-10 rounded-full"
+                  onClick={() => setPage("Home")}
+                ></img>
+                <div className="flex justify-end w-full p-4 md:space-x-20 gap-4 font-mono font-bold items-center md:text-lg text-sm">
+                  <p
+                    onClick={() => {
+                      setPage("Home");
+                    }}
+                    className="hover:border-b-2 transition-all duration-50"
+                  >
+                    Home
+                  </p>
+                  <p
+                    onClick={() => {
+                      setPage("Artworks");
+                    }}
+                    className="hover:border-b-2 transition-all duration-50"
+                  >
+                    Artworks
+                  </p>
+                </div>
+              </div>
+            </nav>
+
             <button
-              className="absolute place-self-center-safe font-bold font-mono text-white border-3 md:text-2xl border-white p-3 rounded-xl hover:scale-105  transition-transform left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 active:scale-90 z-10 duration-100"
+              className="absolute place-self-center-safe font-bold font-mono text-white text-sm md:border-5 border-3 md:text-4xl border-white md:p-5 p-3 rounded-xl hover:scale-105  transition-transform left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 active:scale-90 z-10 duration-100 shadow-2xl"
               onClick={() => {
                 setTimeout(() => setPage("Artworks"), 120);
               }}
               type="button"
             >
-              RAINE'S JOURNEY
+              WORKS OF THE MEWFORD
             </button>
-            <img src="/roughsketch.jpg" className="w-full"></img>
+            <img src="/FINALBANNER.jpg" className="w-full"></img>
           </div>
 
           <div className="md:flex md:w-1/2 place-self-center my-10">
-            <p className="border-l-3 border-gray-400 text-left p-4 md:my-8 font-mono w-[90%] place-self-center md:w-1/2  text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit eos,
-              rem tenetur accusantium alias earum porro. Vitae amet, illo odio
-              eaque repellat molestiae velit quibusdam tenetur sed consequatur
-              suscipit sit! Lorem ipsum dolor, sit amet consectetur adipisicing
-              elit. Inventore harum labore nulla quam distinctio nesciunt
-              necessitatibus omnis itaque possimus, laboriosam incidunt culpa
-              reiciendis amet beatae at. Hic sit quisquam qui? Lorem ipsum dolor
-              sit amet consectetur adipisicing elit. Tempore fugit sint hic,
-              nisi exercitationem debitis reprehenderit id voluptatibus
-              molestiae optio quia aspernatur voluptatem amet ut. Blanditiis est
-              mollitia alias accusantium? lore
-            </p>
+            <div className="md:w-1/2 md:border-l-4 p-4">
+              <h3 className="md:text-2xl text-xl font-mono font-bold mb-2">
+                Some Stuff about my art
+              </h3>
+
+              <p className="text-left  md:mb-8 font-mono  place-self-center   text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+                eos, rem tenetur accusantium alias earum porro. Vitae amet, illo
+                odio eaque repellat molestiae velit quibusdam tenetur sed
+                consequatur suscipit sit! Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Inventore harum labore nulla quam
+                distinctio nesciunt necessitatibus omnis itaque possimus,
+                laboriosam incidunt culpa reiciendis amet beatae at. Hic sit
+                quisquam qui? Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Tempore fugit sint hic, nisi exercitationem debitis
+                reprehenderit id voluptatibus molestiae optio quia aspernatur
+                voluptatem amet ut. Blanditiis est mollitia alias accusantium?
+                lore
+              </p>
+            </div>
+
             <img
               src="/homeart2.jpg"
               className="md:w-1/2 object-cover p-4"
             ></img>
           </div>
 
-          <div className="mb-20 place-self-center md:flex items-center p-6 border-3 rounded-2xl md:w-1/2 border-gray-400 mx-4">
+          <div className="mb-20 place-self-center md:flex items-center p-6 border-3 rounded-2xl md:w-1/2  mx-4 bg-[#d88e8c]">
             <img
-              src="avatar.png"
-              className="md:w-50 md:h-50 w-20 h-20 rounded-full m-8 place-self-center"
+              src="avatar.gif"
+              className="md:w-30 md:h-30 w-20 h-20 rounded-full m-8 place-self-center border-3 md:p-4 bg-white"
             ></img>
             <div>
               <p className="text-left font-bold text-lg md:text-2xl font-mono mb-2">
@@ -91,17 +104,47 @@ function App() {
         </div>
       )}
       {page === "Artworks" && (
-        <div className="pt-15 w-[90%] space-y-8 flex-col  place-self-center text-sm bg-[#F5E7C6]">
-          {Entries.map((entry) => (
-            <ArtEntry
-              key={entry.no}
-              title={entry.title}
-              desc1={entry.desc1}
-              desc2={entry.desc2}
-              imgUrl={entry.imgUrl}
-              date={entry.no}
-            ></ArtEntry>
-          ))}
+        <div className="relative flex-col  place-self-center text-sm bg-[#F5E7C6]">
+          <nav className="w-full fixed bg-transparent z-100  top-0">
+            <div className="flex items-center px-6">
+              <img
+                src="/fis.png"
+                className="active:rotate-360 transition-transform duration-200 w-10 rounded-full"
+                onClick={() => setPage("Home")}
+              ></img>
+              <div className="flex justify-end w-full p-4 md:space-x-20 gap-4 font-mono font-bold items-center md:text-lg text-sm">
+                <p
+                  onClick={() => {
+                    setPage("Home");
+                  }}
+                  className="hover:border-b-2 transition-all duration-50"
+                >
+                  Home
+                </p>
+                <p
+                  onClick={() => {
+                    setPage("Artworks");
+                  }}
+                  className="hover:border-b-2 transition-all duration-50"
+                >
+                  Artworks
+                </p>
+              </div>
+            </div>
+          </nav>
+
+          <div className="md:p-8 p-2 md:pt-30 pt-20 md:space-y-5 md:space-x-5 md:grid md:grid-cols-2">
+            {Entries.map((entry) => (
+              <ArtEntry
+                key={entry.no}
+                title={entry.title}
+                desc1={entry.desc1}
+                desc2={entry.desc2}
+                imgUrl={entry.imgUrl}
+                date={entry.no}
+              ></ArtEntry>
+            ))}
+          </div>
 
           <div className="p-5"></div>
         </div>
--- src/components/artEntry.jsx
+++ src/components/artEntry.jsx
@@ -1,35 +1,45 @@
 import { useState } from "react";
 export default function ArtEntry({ imgUrl, desc1, desc2, title, date }) {
-  
-  const [logs,setLogs] = useState(false);
-  
-  return (
-    <div>
-      <div className="pb-4">
-        <img src={imgUrl} className="shadow-md border-2 border-gray-300"></img>
-      </div>
+  const [logs, setLogs] = useState(false);
+  const [hover, setHover] = useState(false);
 
-      <div className="flex items-center justify-between">
+  return (
+    <div
+      className="hover:scale-102 transition-transform duration-200"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div className="pb-4 relative">
+        <img src={imgUrl} className="shadow-md border-2 border-gray-300"></img>
+        {hover && <p className="transition-transform duration-200 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-bold  text-4xl font-mono text-white shadow-2xl">{title}</p>
+     } </div>
+
+      {/**   <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
-          <img src="/avatar.png" className="w-10 h-10 rounded-full"></img>
           <p className="font-mono md:text-xl text-md font-bold">{title}</p>
         </div>
 
         <div className="flex items-center gap-2">
           <p className="font-mono">{date}</p>
 
-          <button className="border-2 rounded-lg p-1 active:scale-90 transition-transform"
-          type="button"
-          onClick={()=> setLogs(!logs)}
-          >Read Logs</button>
+          <button
+            className="border-2 rounded-lg p-1 active:scale-90 transition-transform"
+            type="button"
+            onClick={() => setLogs(!logs)}
+          >
+            Read Logs
+          </button>
         </div>
       </div>
 
       <div className=" px-4 font-serif md:flex gap-4 mt-2 text-xs md:text-sm bg-gray-300 p-1 shadow-md">
-        {logs && <p className="md:border-none border-l-2 border-gray-400 px-4 mt-2 h-50 overflow-auto">
-          {desc1}
-        </p>}
+        {logs && (
+          <p className="md:border-none border-l-2 border-gray-400 px-4 mt-2 h-50 overflow-auto">
+            {desc1}
+          </p>
+        )}
       </div>
+      */}
     </div>
   );
 }
